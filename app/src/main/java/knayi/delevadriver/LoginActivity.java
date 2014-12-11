4261f2355538dec3f8300a0e27526df1822578ad
Hein Htet <heinhtetaunghtetaung92@gmail.com>
2014-12-11 18:11:20 +0630

    first commit

---
+++ app/src/main/java/knayi/delevadriver/LoginActivity.java
@@ -0,0 +1,15 @@
+package knayi.delevadriver;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends ActionBarActivity {
+
+
+
+}
